38650aa6590f6770342673a08fc4a5b02281cb69
oxbowbet <118590025+oxbowbet@users.noreply.github.com>
2022-11-19 00:50:01 -0800

    Add files via upload
    Signed-off-by: oxbowbet <118590025+oxbowbet@users.noreply.github.com>

---
+++ main.js
@@ -0,0 +1,37 @@
+function fixture() {
+	let data="<span>&nbsp&nbsp&nbsp3.5</span><br><span>Arsenal</span>";
+document.getElementById('teamA').innerHTML=data;
+let data1="<span>&nbsp&nbsp&nbsp2.5</span><br><span>Mancity</span>";
+document.getElementById('teamB').innerHTML=data1;
+document.getElementById('teamA').style.display="block"
+document.getElementById('teamB').style.display="block"
+}
+function profilemenu() {
+	document.getElementById('profile-menu').style.display="block"
+	document.getElementById('livesoccer').style.display="none"
+	document.getElementById('profile').style.display="none"
+	document.getElementById('profile1').style.display="block"
+}
+function profilemenu1() {
+	document.getElementById('profile-menu').style.display="none"
+	document.getElementById('livesoccer').style.display="block"
+	document.getElementById('profile').style.display="block"
+	document.getElementById('profile1').style.display="none"
+}
+function pay() {
+	confirm('you are leaving this app to proceed with payments');
+	open('https://buy.stripe.com/test_cN28wD3RpflaebKdQS')
+}
+function pay1() {
+	confirm('you are leaving this app to proceed with payments');
+	open('https://buy.stripe.com/test_cN28wD3RpflaebKdQS')
+}
+function withdraw() { 
+	prompt('what your username')
+	confirm("YOUR REQUEST HAS BEEN SENT TO OXBOW BET .\n PLEASE WAIT WHILE WE CONFIRM PAYMENTS.\n THIS CAN TAKE UPTO 24HOURS \(ONE DAY\).\nTHANK YOU FOR YOUR PATIENCE!")
+}
+function image() {
+	let image=document.getElementById('imgsrc');
+		image.display();
+	document.getElementById('image').innerHTML=image;
+}
